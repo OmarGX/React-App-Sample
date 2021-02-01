9f5b19150f6f76346f5fdf9f877fba8bdeeb298b
Omar Ghetti <omarghetti2@gmail.com>
2021-02-01 11:27:31 +0100

    lazy loading for FilterSpace

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
-import React from "react";
-import HeaderBar from "./Components/HeaderBar";
-import FilterSpace from "./Components/FilterSpace";
+import React, { lazy, Suspense } from "react";
 import { ThemeProvider } from "@material-ui/styles"
 import theme from "./Themes/Theme";
+import HeaderBar from "./Components/HeaderBar";
+
+const FilterSpace = lazy(() => import('./Components/FilterSpace'));
 
 function App() {
   return (
       <ThemeProvider theme={theme}>
+          <HeaderBar></HeaderBar>
+          <Suspense fallback={<div>loading...</div>}>
+          <link rel="preconnect" href="https://gorest.co.in/public-api/products/"/>
           <div className="App">
-              <HeaderBar></HeaderBar>
               <FilterSpace></FilterSpace>
           </div>
+          </Suspense>
       </ThemeProvider>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,11 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import React from "react";
 
-test('renders learn react link', () => {
+test('renders HeaderBar', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/Filter Products/i);
   expect(linkElement).toBeInTheDocument();
 });
+
+
